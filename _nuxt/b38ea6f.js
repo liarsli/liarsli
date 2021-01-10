0d5c59163f1d9248975b560cf8454dfd36a85950
MukhutdinovRR <mukhutdinov.ruslan@gmail.com>
2021-01-10 18:26:46 +0500

    Updates

---
+++ _nuxt/b38ea6f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{168:function(t,e,n){"use strict";n.r(e);var r=n(14),component=Object(r.a)({},(function(){var t=this.$createElement;this._self._c;return this._m(0)}),[function(){var t=this.$createElement,e=this._self._c||t;return e("section",[e("h1",[this._v("Portfolio")])])}],!1,null,null,null);e.default=component.exports}}]);
